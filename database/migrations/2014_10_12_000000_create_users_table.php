fd4f0e0cd6c16837c0f612f1e79e1c76eed1c9b2
ralphkian <ralph.kjian@hotmail.com>
2024-10-10 23:54:36 +0300

    Changed the id inital number

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -12,13 +13,16 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id()->unsigned(); // Laravel automatically creates the 'id' as an unsigned bigint
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
+
+        // Set the starting value for the 'id' column
+        DB::statement('ALTER TABLE users AUTO_INCREMENT = 1100;');
     }
 
     /**
